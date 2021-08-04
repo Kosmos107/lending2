81d401395c07ab39b6eebf4f0c27944bba32bc11
kosmos107 <strateg87@gmail.com>
2021-08-04 17:36:23 +0300

    готово меню

--- #src/js/script.js
+++ #src/js/script.js
@@ -17,3 +17,44 @@ btn.addEventListener("click",(e)=>{
         console.log(input[i].value)
     }
 })
+
+
+
+///функция работы с меню
+const massPrice=[]
+for(let i =0;i<3;i++){
+    massPrice[i]=document.querySelector("#price"+i)
+}
+console.log(massPrice)
+
+let pizza = "PIZZA QUATRO STAGIONI  . . . ."
+let coffe = "coffee QUATRO STAGIONI  . . . ."
+let chi = "chi QUATRO STAGIONI  . . . ."
+let loh = "loh QUATRO STAGIONI  . . . ."
+
+function addMenu(n,z="55,68 usd",x){
+for(let i=0;i<8;i++){
+    
+    let inner = document.createElement('div');
+    let title =document.createElement('p')
+    let subtitle =document.createElement('p')
+    let money =document.createElement('span')
+    money.className="block-price__money"
+    money.textContent=z
+    title.className="block-price__title"
+    subtitle.className="block-price__subtitle"
+    inner.className="block-price__inner"
+    title.textContent=x
+    subtitle.textContent="Integer ullamcorper neque eu purus euismod"
+    inner.append(title)
+    title.append(money)
+    inner.append(subtitle)
+    
+    massPrice[n].append(inner)
+    
+}
+}
+
+// addMenu(0,22,loh)
+// addMenu(1,44,coffe)
+// addMenu(2,55)
